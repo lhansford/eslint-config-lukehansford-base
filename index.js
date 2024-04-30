d338975845178792759c16b481c8b0c0f5f26c95
Luke Hansford <mail@lukehansford.me>
2024-04-30 08:47:58 +0700

    Update to eslint 9

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-/* eslint-env node */
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/stylistic',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'prettier',
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
-    ],
-    curly: 'error',
-    'import/first': 'error',
-    'import/order': ['error', { 'newlines-between': 'always' }],
-    'no-alert': 'error',
-    semi: 'error',
+import tsParser from '@typescript-eslint/parser';
+import tsLintPlugin from '@typescript-eslint/eslint-plugin';
+import js from "@eslint/js";
+import { configs as importConfigs } from 'eslint-plugin-import'
+// import {} from 'eslint-config-prettier'; // TODO:
+
+export default [
+  js.configs.recommended,
+  tsLintPlugin.configs.recommended,
+  tsLintPlugin.configs.stylistic,
+  importConfigs.recommended,
+  importConfigs.typescript,
+  // // 'prettier', // TODO:
+  {
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: { '@typescript-eslint': tsLintPlugin },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+      ],
+      curly: 'error',
+      'import/first': 'error',
+      'import/order': ['error', { 'newlines-between': 'always' }],
+      'no-alert': 'error',
+      semi: 'error',
+    },
   },
-};
+];
