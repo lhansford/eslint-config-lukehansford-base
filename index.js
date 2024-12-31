94b1b86aa70daf71c8be238c985bf2101a83d40f
Luke Hansford <mail@lukehansford.me>
2025-01-01 06:19:29 +0800

    Update to eslint 9

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-/* eslint-env node */
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/stylistic',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'prettier',
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
-    ],
-    curly: 'error',
-    'import/first': 'error',
-    'import/order': ['error', { 'newlines-between': 'always' }],
-    'no-alert': 'error',
-    semi: 'error',
-  },
-};
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [importPlugin.flatConfigs.recommended],
+  }
+);
